50b7ca1d30b7d3799f3dd34df9d9d4cd5651e7f6
Kevin <kevin.shehan30@gmail.com>
2023-12-03 00:12:41 +0530

    nav link textdecoration removed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -14,14 +14,14 @@ const Navbar = () => {
                 <p>SHOPPER</p>
             </div>
             <ul className="nav-menu">
-                <li onClick={()=>{setMenu("shop")}}><Link to='/'> Shop</Link> {menu==="shop"?<h1/>:<></>} </li>
-                <li onClick={()=>{setMenu("mens")}}><Link to=''>Men</Link>{menu==="mens"?<h1/>:<></>}</li>
-                <li onClick={()=>{setMenu("womens")}}><Link to=''>Women</Link>{menu==="womens"?<h1/>:<></>}</li>
-                <li onClick={()=>{setMenu("kids")}}><Link to=''>Kids</Link>{menu==="kids"?<h1/>:<></>}</li>
+                <li onClick={()=>{setMenu("shop")}}><Link to='/' style={{textDecoration:'none'}}> Shop</Link> {menu==="shop"?<h1/>:<></>} </li>
+                <li onClick={()=>{setMenu("mens")}}><Link to='/mens' style={{textDecoration:'none'}}>Men</Link>{menu==="mens"?<h1/>:<></>}</li>
+                <li onClick={()=>{setMenu("womens")}}><Link to='/womens' style={{textDecoration:'none'}}>Women</Link>{menu==="womens"?<h1/>:<></>}</li>
+                <li onClick={()=>{setMenu("kids")}}><Link to='/kids' style={{textDecoration:'none'}}>Kids</Link>{menu==="kids"?<h1/>:<></>}</li>
             </ul>
             <div className="nav-login-cart">
-                <button>Login</button>
-                <img src={cart_icon} alt="" />
+                <Link to='/login'><button>Login</button></Link>
+                <Link to='/cart' style={{textDecoration:'none'}}><img src={cart_icon} alt="" /></Link>
                 <div className="nav-cart-count">0</div>
             </div>
         </div>
