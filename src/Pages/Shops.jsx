dd459e5d47466ca138b355f1a66a1c2a0ed5f028
Kevin <kevin.shehan30@gmail.com>
2023-12-09 16:36:25 +0530

    Footer comp css added

--- src/Pages/Shops.jsx
+++ src/Pages/Shops.jsx
@@ -4,7 +4,6 @@ import Popular from '../Components/Popular/Popular'
 import Offers from '../Components/Offers/Offers';
 import NewCollections from '../Components/NewCollections/NewCollections';
 import NewsLetter from '../Components/NewsLetter/NewsLetter';
-import Footer from '../Components/Footer/Footer';
 
 const Shops = () => {
   return (
@@ -14,7 +13,6 @@ const Shops = () => {
       <Offers />
       <NewCollections />
       <NewsLetter/>
-      <Footer/>
     </div>
   )
 }
