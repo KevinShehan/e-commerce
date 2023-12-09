8953b3c9241d4864ed4317a77ee41d3eda1e1d6d
Kevin <kevin.shehan30@gmail.com>
2023-12-09 15:31:55 +0530

    NewsLetter Components Created

--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import "./NewsLetter.css";
 
 const NewsLetter = () => {
   return (
-    <div>
-      
+    <div className='newsletter'>
+      <h1>Get Exclusive Offers On Your Email</h1>
+      <p>Subscribe to Our NewLetter and Stay Updated</p>
+      <div>
+        <input type="email" name="" id="" placeholder='Your Email ID' />
+        <button>Subscribe</button>
+      </div>
     </div>
   )
 }
