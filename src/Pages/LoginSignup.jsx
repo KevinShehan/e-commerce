a0d9d5ed1479af03f7bf5d8eb0f3f8b5b9e46659
Kevin <kevin.shehan30@gmail.com>
2023-12-10 20:42:31 +0530

    loginsignup page comp created

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const LoginSignup = () => {
   return (
-    <div>
-      
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        
+      </div>
     </div>
   )
 }
