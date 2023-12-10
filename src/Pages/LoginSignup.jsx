b44cf338f45b2e9fff555d30b3a150a6b984f0d2
Kevin <kevin.shehan30@gmail.com>
2023-12-10 20:50:13 +0530

    loginsignup html structure created

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -4,7 +4,20 @@ const LoginSignup = () => {
   return (
     <div className='loginsignup'>
       <div className="loginsignup-container">
-        
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" name="" id="" placeholder='Your Name' />
+          <input type="email" name="" id="" placeholder='Your Email' />
+          <input type="password" name="" id="" placeholder='Your Password' />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">
+          Already have an account? <span>Login</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>BY continueing, I agree to the terms of use and privacy policy</p>
+        </div>
       </div>
     </div>
   )
